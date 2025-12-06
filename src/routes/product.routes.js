1fdfbb12b42fc5937322f9f65f0395fcb00346bb
sanofficial825-dev <sanofficial825@gmail.com>
2025-12-06 15:38:19 +0530

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -5,8 +5,7 @@ const router = Router();
 
 // router.get("/", getAllProducts);         // GET /api/products
 router.get("/", getProducts); // GET /api/products?brandName=someBrand
-router.get("/", getProductNames); // GET /api/products?brandName=someBrand
-
 router.get("/:id", getProductById);      // GET /api/products/:id
 
 export default router;
+
