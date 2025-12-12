a7b20b0bb81a7b70482620a2d51de282e4aa1835
sanofficial825-dev <sanofficial825@gmail.com>
2025-12-12 16:32:21 +0530

    Update product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { getAllProducts, getProductById, getProducts } from "../controllers/product.controller.js";
+import { getAllProducts, getProductById, getProducts, getProducts2, getProductNamesByBrandName, getProductsByCategory } from "../controllers/product.controller.js";
 
 const router = Router();
 
-// router.get("/", getAllProducts);         // GET /api/products
 router.get("/", getProducts); // GET /api/products?brandName=someBrand
-router.get("/:id", getProductById);      // GET /api/products/:id
+router.get("/productNames", getProductNamesByBrandName); // GET /api/products/names?brandName=someBrand
+
+// router.get("/2", getProducts2); // GET /api/products?brandName=someBrand
+// router.get("/getProductsByCategory", getProductsByCategory); // GET /api/products/names?brandName=someBrand
+// router.get("/:id", getProductById);      // GET /api/products/:id
+// router.get("/", getAllProducts);         // GET /api/products
 
 export default router;
-
