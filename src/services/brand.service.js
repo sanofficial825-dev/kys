95a20499edff0faea5a763134a8a2b2ecae89613
sanofficial825-dev <sanofficial825@gmail.com>
2025-12-18 18:44:36 +0530

    Update brand.service.js

--- src/services/brand.service.js
+++ src/services/brand.service.js
@@ -1,7 +1,19 @@
 import { pool } from "../config/db.js";
 
-export async function getAllBrands() {
-    const result = await pool.query("select distinct brand_name from kys_brand_products");
+export async function getAllBrands(search) {
+    let query = `select distinct brand_name from kys_brand_products`;
+
+    const values = [];
+
+    if (search && search.trim()) {
+        values.push(`%${search}%`);
+        query += ` WHERE brand_name ILIKE $1`;
+    }
+
+    query += ` ORDER BY brand_name`;
+
+    const result = await pool.query(query, values);
+
     return result.rows.map(row => row.brand_name);
 }
 
